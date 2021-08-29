644906363b0ea0cb91de4515ab98d372f0b4273b
Jason Watmore <cornflourblue@hotmail.com>
2021-08-29 20:04:40 +1000

    added links to tutorial

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Next.js 11 - Basic HTTP Authentication Example
 
-Documentation coming soon!
+Documentation and live demo available at https://jasonwatmore.com/post/2021/08/29/next-js-basic-http-authentication-tutorial-with-example-app
--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ function App({ Component, pageProps }) {
             {/* credits */}
             <div className="text-center mt-4">
                 <p>
-                    <a href="#" target="_top">Next.js 11 - Basic Authentication Tutorial with Example App</a>
+                    <a href="https://jasonwatmore.com/post/2021/08/29/next-js-basic-http-authentication-tutorial-with-example-app" target="_top">Next.js 11 - Basic Authentication Tutorial with Example App</a>
                 </p>
                 <p>
                     <a href="https://jasonwatmore.com" target="_top">JasonWatmore.com</a>
